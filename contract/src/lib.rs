a36fd376f5a0908e6ee58debb18b1dfdd1f91de1
Roman <zajic@zajic.net>
2022-09-13 16:18:38 +0800

    Contract: tests added, string values

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -46,7 +46,7 @@ impl StickyHabits {
 
         self.habits.iter()
             .skip(from as usize)
-            .take(limit.unwrap_or(10) as usize)
+            .take(limit.unwrap_or(7) as usize)
             .collect()
     }
 
@@ -66,27 +66,55 @@ impl StickyHabits {
  * The rest of this file holds the inline tests for the code above
  * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
  */
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn get_default_greeting() {
-        let contract = StickyHabits::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
+    fn add_habit() {
+        let mut contract = StickyHabits::default();
+        contract.add_habit(
+            "Clean my keyboard once a week".to_string(),
+            "2022/09/30".to_string(),
+            "50".to_string(),
+            "Adam".to_string(),
         );
+
+        let posted_habit = &contract.get_habits(None, None)[0];
+        assert_eq!(posted_habit.description, "Clean my keyboard once a week".to_string());
+        assert_eq!(posted_habit.penalty, "50".to_string());
     }
 
     #[test]
-    fn set_then_get_greeting() {
+    fn iterates_habits() {
         let mut contract = StickyHabits::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
+        contract.add_habit(
+            "Clean my keyboard once a week".to_string(),
+            "2022/09/30".to_string(),
+            "50".to_string(),
+            "Adam".to_string(),
         );
+        contract.add_habit(
+            "Eat one tomato every day".to_string(),
+            "2022/09/20".to_string(),
+            "150".to_string(),
+            "Lubos".to_string(),
+        );
+        contract.add_habit(
+            "Exercise without smartphone".to_string(),
+            "2022/10/31".to_string(),
+            "250".to_string(),
+            "Arc".to_string(),
+        );
+
+
+        let habits = &contract.get_habits(None, None);
+        assert_eq!(habits.len(), 3);
+
+        let last_habit = &contract.get_habits(Some(U128::from(1)), Some(2))[1];
+        assert_eq!(last_habit.deadline, "2022/10/31".to_string());
+        assert_eq!(last_habit.beneficiary, "Arc".to_string());
     }
 }
+
