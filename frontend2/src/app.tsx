3b93e8346636b0e57a7d4a4e589ff3547e478cca
Roman <zajic@zajic.net>
2022-12-21 17:33:35 +0800

    Frontend2: make listHabits() synchronous, near dependencies

--- frontend2/src/app.tsx
+++ frontend2/src/app.tsx
@@ -15,8 +15,12 @@ import Header from "./components/header";
 import MainSection from "./components/main-section";
 import "todomvc-app-css/index.css";
 
+
+
 // This is the top-level component for our app.
 const App = ({ rep }: { rep: Replicache<M> }) => {
+  console.log("Test test testu")
+
   // Subscribe to all todos and sort them.
   const todos = useSubscribe(rep, listTodos, [], [rep]);
   todos.sort((a, b) => a.sort - b.sort);
--- frontend2/src/habit.ts
+++ frontend2/src/habit.ts
@@ -2,6 +2,7 @@
 // function to get all Habits.
 
 import { StickyHabits } from "./near-interface";
+import HabitList from "./components/habit-list";
 
 export type Habit = {
   description: string,
@@ -15,6 +16,10 @@ export type Habit = {
 
 export type HabitUpdate = Partial<Habit> & Pick<Habit, "description">;
 
-export async function listHabits(contract: StickyHabits) {
-  return await contract.getUserHabits();
+export function listHabits(contract: StickyHabits) {
+  let result: Habit[] = [];
+  contract.getUserHabits().then(res => {
+    result = res;
+  });
+  return result
 }
